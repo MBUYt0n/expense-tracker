e8fdf59370565704f224b8d9f27e5699a6741ef3
MBUYt0n <shusrith03@gmail.com>
2024-05-19 21:44:06 +0530

    .

--- script.sh
+++ script.sh
@@ -2,4 +2,8 @@ output=$(python3 driver.py)
 if [ "$output" = "1" ]; then
     python3 csv-getter.py
     jupyter nbconvert --execute --inplace stats.ipynb
+    code stats.ipynb
+    git add --all
+    git commit -m "."
+    git push
 fi
