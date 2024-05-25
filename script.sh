9219bc446a9998443246c67a14d030462350e32b
MBUYt0n <shusrith03@gmail.com>
2024-05-26 00:58:11 +0530

    .

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 output=$(python3 driver.py)
 if [ "$output" = "1" ]; then
+    cd projects/expense-tracker
     python3 csv-getter.py
     jupyter nbconvert --execute --inplace stats.ipynb
     jupyter nbconvert --to html stats.ipynb --output index.html
     code stats.ipynb
     git add --all
     git commit -m "."
-    git remote set-url origin https://MBUYt0n:$GITHUB_TOKEN@github.com/MBUYt0n/expense-tracker.git
     git remote set-url origin https://MBUYt0n:$GITHUB_TOKEN@github.com/MBUYt0n/expense-tracker.git
     git push
 fi
