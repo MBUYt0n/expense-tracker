518d204a9d43b6d0713a76132a67134ca65613e2
MBUYt0n <shusrith03@gmail.com>
2024-07-06 18:49:07 +0530

    .

--- pusher.sh
+++ pusher.sh
@@ -1,3 +1,3 @@
-export GITHUB_TOKEN=$(cat /home/shusrith/key.txt)
+export GITHUB_TOKEN=$(cat /home/shusrith/scripts/personal-access-token.txt)
 git remote set-url origin https://MBUYt0n:$GITHUB_TOKEN@github.com/MBUYt0n/expense-tracker.git
 git push
