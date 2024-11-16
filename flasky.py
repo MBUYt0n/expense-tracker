f00cdd9e33789613ff75b194d67ff827e524a6b9
MBUYt0n <shusrith03@gmail.com>
2024-11-16 11:56:28 +0530

    well well well

--- flasky.py
+++ flasky.py
@@ -1,18 +1,68 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
+from nbconvert import HTMLExporter
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
+import csv
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def home():
+    return render_template("home.html")
+
+
+@app.route("/input")
+def input_screen():
     return render_template("input.html")
 
-@app.route("/input", methods=["POST"])
-def input():
+
+@app.route("/api/input", methods=["POST"])
+def input_api():
     data = request.get_json()
     print(data)
-    return "x"
+    with open("expenses.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(
+            [
+                data["date"],
+                data["amount"],
+                data["item"],
+                data["category"],
+                data["split"],
+                data["notme"],
+            ]
+        )
+    return jsonify(data)
+
+
+@app.route("/api/done", methods=["POST"])
+def ipynb():
+    notebook_filename = "stats.ipynb"
+    with open(notebook_filename, "r", encoding="utf-8") as f:
+        notebook = nbformat.read(f, as_version=4)
+
+    executor = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    executor.preprocess(notebook, {"metadata": {"path": "./"}})
+
+    with open(notebook_filename, "w", encoding="utf-8") as f:
+        nbformat.write(notebook, f)
+
+    html_exporter = HTMLExporter()
+    body, _ = html_exporter.from_notebook_node(notebook)
+
+    with open("templates/index.html", "w", encoding="utf-8") as f:
+        f.write(body)
+
+    return jsonify("done")
+
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(port=3000, debug=True)
